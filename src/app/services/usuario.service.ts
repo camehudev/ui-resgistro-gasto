66d44bc97d3e339daaa187f59894db899bf1bc43
Carlos Humberto <camehudev@gmail.com>
2026-07-30 00:07:24 -0300

    environments para usuarios

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,29 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
   //private apiUrl = 'https://pessoal-proj-java-registro.sjj3wv.easypanel.host/api/usuarios';
-  private apiUrl = 'http://localhost:5000';
+  private baseURL = `${environment.apiUrl}`
 
 
   constructor(private http: HttpClient) {}
 
   listar(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(this.apiUrl, { withCredentials: true });
+    return this.http.get<Usuario[]>(this.baseURL, { withCredentials: true });
   }
 
   salvar(usuario: Usuario): Observable<Usuario> {
     if (usuario.id) {
-      return this.http.put<Usuario>(`${this.apiUrl}/${usuario.id}`, usuario, { withCredentials: true });
+      return this.http.put<Usuario>(`${this.baseURL}/${usuario.id}`, usuario, { withCredentials: true });
     }
-    return this.http.post<Usuario>(this.apiUrl, usuario, { withCredentials: true });
+    return this.http.post<Usuario>(this.baseURL, usuario, { withCredentials: true });
   }
 
   deletar(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`, { withCredentials: true });
+    return this.http.delete<void>(`${this.baseURL}/${id}`, { withCredentials: true });
   }
 }
