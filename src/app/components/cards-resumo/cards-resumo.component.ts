df4b9422f4bcc0aebb9cb1afaa37718d8edf0626
Carlos Humberto <camehudev@gmail.com>
2026-08-02 21:59:42 -0300

    Ajuste tela de login e card

--- src/app/components/cards-resumo/cards-resumo.component.ts
+++ src/app/components/cards-resumo/cards-resumo.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
 
 @Component({
   selector: 'app-cards-resumo',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './cards-resumo.component.html',
   styleUrl: './cards-resumo.component.css'
 })
