116c871faae1783ca7695931bfac62b0de78f68b
Carlos Humberto <camehudev@gmail.com>
2026-08-10 13:55:24 -0300

    Rebase UI 10-08-2026

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,25 +1,42 @@
+<<<<<<< HEAD
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
+=======
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Importação essencial
+>>>>>>> b861126 (Atualização page login)
 import { DividerModule } from 'primeng/divider';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+<<<<<<< HEAD
 import { AuthService } from '../../services/auth.service';
+=======
+
+>>>>>>> b861126 (Atualização page login)
 
 @Component({
   selector: 'app-login',
   standalone: true,
+<<<<<<< HEAD
   imports: [DividerModule, ButtonModule, InputTextModule, ReactiveFormsModule],
+=======
+  imports: [DividerModule, ButtonModule, InputTextModule, ReactiveFormsModule,],
+>>>>>>> b861126 (Atualização page login)
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
+<<<<<<< HEAD
   // Utilizando injeção moderna via inject() de forma padronizada
   private router = inject(Router);
   private authService = inject(AuthService);
+=======
+>>>>>>> b861126 (Atualização page login)
 
   formGroup!: FormGroup;
 
+<<<<<<< HEAD
   ngOnInit() {
     this.formGroup = new FormGroup({
       email: new FormControl<string | null>(null),
@@ -43,4 +60,21 @@ export class LoginComponent implements OnInit {
       }
     });
   }
+=======
+  formGroup!: FormGroup;
+
+  showDashboard() {
+    // Implement your logic to show the dashboard here
+    //this.router.navigate(['/dashboard']);
+    console.log(this.formGroup.value);
+  }
+
+  ngOnInit() {
+        this.formGroup = new FormGroup({
+            username: new FormControl<string | null>(null),
+            password: new FormControl<string | null>(null)
+        });
+    }
+
+>>>>>>> b861126 (Atualização page login)
 }
