e58ef4588c5237437f9b175aebad0ae3d4997878
Carlos Humberto <camehudev@gmail.com>
2026-08-10 14:21:51 -0300

    Criado botão adicionar novo registro no recebimento

--- src/app/pages/recebimento/recebimento.component.ts
+++ src/app/pages/recebimento/recebimento.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-recebimento',
   standalone: true,
-  imports: [],
+  imports: [CalendarModule, FormsModule,
+        DropdownModule,
+        DialogModule,
+        ButtonModule,
+        InputTextModule,
+        ReactiveFormsModule,
+        ToastModule,
+        CheckboxModule,
+        CommonModule,
+        ],
   templateUrl: './recebimento.component.html',
   styleUrl: './recebimento.component.css'
 })
-export class RecebimentoComponent {
+export class RecebimentoComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+
+  visibleDialogRecebimento:boolean=false
+  formGroupGasto!: FormGroup;
+  categoriaSaldo:any[]=[];
+
+  showDialogRecebimento(){
+    this.visibleDialogRecebimento = true
+  }
+
+  registrarParcelamento(){
+
+  }
 
 }
