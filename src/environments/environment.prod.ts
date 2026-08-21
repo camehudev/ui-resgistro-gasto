300cccdbd2a19d2cd81be065e7aba7438120cc36
Carlos Humberto <camehudev@gmail.com>
2026-08-21 12:13:29 -0300

    Ajustes de variaveis de ambiente FIX

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // Se estiver rodando local, certifique-se de usar http e não https:
+  apiUrl: 'https://pessoal-proj-java-registro.sjj3wv.easypanel.host'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  // Se estiver rodando local, certifique-se de usar http e não https:
+  apiUrl: 'http://localhost:5000'
+};
